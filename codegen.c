03a0fe9063ccb4b766ae93c3b3733780ef6b8afb
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2023-07-28 20:49:52 +0900

    関数の定義場所を変更

--- codegen.c
+++ codegen.c
@@ -7,6 +7,26 @@ bool at_eof() {
   return token->kind == TK_EOF;
 }
 
+// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
+// それ以外の場合にはエラーを報告する。
+void expect(char *op) {
+  if (token->kind != TK_RESERVED ||
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len)) {
+    error_at(token->str, "\"%s\"ではありません", op);
+  }
+  token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
+// それ以外の場合にはエラーを報告する。
+int expect_number() {
+  if (token->kind != TK_NUM) error_at(token->str, "数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
   Node *node = calloc(1, sizeof(Node));
   node->kind = kind;
